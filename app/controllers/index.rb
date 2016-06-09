ed770e12bfaad9f0b87b2792e3331ec68500a0c3
Sneha Narasimhan & Olya Royall <pair+snehabn.venture-vin@devbootcamp.com>
2016-06-09 16:14:09 -0700

    update migration and seed to store json files, to cache api request

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,8 @@
 get '/' do
 VoteSmart.api_key = ENV['SECRET_KEY']
-@candidates = VoteSmart::Candidate.get_by_zip(94117)['candidateList']['candidate']
-@local_officials = VoteSmart::Local.request("Local.getOfficials", "localId" => "213")['candidateList']['candidate']
+# @candidates = VoteSmart::Candidate.get_by_zip(94117)['candidateList']['candidate']
+# @local_officials = VoteSmart::Local.request("Local.getOfficials", "localId" => "213")['candidateList']['candidate']
+@candidates = Candidate.candidates
+@local_candidates = Candidate.local_candidates
   erb :index
 end
